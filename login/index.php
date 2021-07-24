4837ab4b242a76427602d433e725bc0538f369ef
SatyamG3001 <satyamgupta3001@gmail.com>
2021-07-24 21:09:10 +0530

    Create index.php

---
+++ login/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>MANAGER LOGIN</title>
+    <style>
+    </style>
+</head>
+<body style="background-image: url('h1.jpg');">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 m-auto">
+                <div class="card bg-dark mt-5">
+                    <div class="card-title bg-primary text-white mt-5">
+                        <h3 class="text-center py-3">MANAGER LOGIN</h3>
+                    </div>
+
+                    <?php 
+                        if(@$_GET['Empty']==true)
+                        {
+                    ?>
+                        <div class="alert-light text-danger text-center py-3"><?php echo $_GET['Empty'] ?></div>                                
+                    <?php
+                        }
+                    ?>
+
+
+                    <?php 
+                        if(@$_GET['Invalid']==true)
+                        {
+                    ?>
+                        <div class="alert-light text-danger text-center py-3"><?php echo $_GET['Invalid'] ?></div>                                
+                    <?php
+                        }
+                    ?>
+
+                    <div class="card-body">
+                        <form action="process.php" method="post">
+                            <input type="text" name="email" placeholder=" User Name" class="form-control mb-3">
+                            <input type="password" name="Password" placeholder=" Password" class="form-control mb-3">
+                            <button class="btn btn-success mt-3" name="Login">Login</button>
+                            <a href="javascript:history.back()" class="btn btn-success mt-3">Back</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
